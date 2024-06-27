fe9fbac9834d98cce9c8f57ad451e044ae577658
Daniel Hernandez <dh893177@gmail.com>
2024-06-27 10:44:40 -0500

    added styling

--- posts/posts.js
+++ posts/posts.js
@@ -16,42 +16,48 @@ function getLoginData() {
 
 
 const getPostsAsyncExample = async () => {
+    try {
 
-    const loginData = getLoginData();
+        const loginData = getLoginData();
 
-    const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
-        method: "GET",
-        headers: {
-            // This header is how we authenticate our user with the
-            // server for any API requests which require the user
-            // to be logged-in in order to have access.
-            // In the API docs, these endpoints display a lock icon.
-            Authorization: `Bearer ${loginData.token}`
+        const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
+            method: "GET",
+            headers: {
+                // This header is how we authenticate our user with the
+                // server for any API requests which require the user
+                // to be logged-in in order to have access.
+                // In the API docs, these endpoints display a lock icon.
+                Authorization: `Bearer ${loginData.token}`
+            }
+        })
+
+        const data = await response.json();
+        //do something with the posts
+        console.log(data, "using async/await")
+
+        // document.querySelector("#postsContainer").innerHTML = JSON.stringify(data)
+        const postsContainer = document.getElementById('postsContainer');
+
+        for (const post of data) {
+            const postElement = document.createElement('div');
+            postElement.className = 'post';
+
+            const postContent = `
+            <h3>${post.text}</h3>
+            <p class="author"><strong>@</strong> ${post.username}</p>
+            <p class="timestamp"><strong>Date:</strong> ${new Date(post.createdAt).toLocaleString()}</p>
+             `;
+
+            postElement.innerHTML = postContent;
+            postsContainer.appendChild(postElement);
         }
-    })
+    } catch (error) {
+        console.error('Error:', error);
+    }
 
-    const data = await response.json();
-
-    //do something with the posts
-    console.log(data, "using async/await")
-
-
-    const displayElement = document.getElementById('postsContainer');
-
-    // Create a string or element to display the data
-    // Here is an example for a list of items
-    let html = '<ul>';
-    data.forEach(item => {
-        html += `
-            <div><p class="author"><strong>@</strong>${item.username}</p>
-            <h3>${item.text}</h3>
-            <p class="timestamp"><strong>Date:</strong> ${new Date(item.createdAt).toLocaleString()}</p>
-            </div>`;
-    });
-    html += '</ul>';
-
-    displayElement.innerHTML = html;
 }
+
+
 
 function logout() {
     const loginData = getLoginData();
